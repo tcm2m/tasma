26c1dc53ec874d2f7182536cbaa5f4d4764ead90
Erhan Abay <erhanabay@gmail.com>
2013-05-09 09:14:23 +0300

    doglocation store'daki koordinat bilgileri ile köpeğin izlediği yol haritaya polyline olarak çizdirildi.

--- www/app/controller/Map.js
+++ www/app/controller/Map.js
@@ -1,22 +1,32 @@
 Ext.define('Tasma.controller.Map', {
     extend: 'Ext.app.Controller',
-    
+
     config: {
         routes: {
             '': 'showMap'
         },
         refs: {
             mapView: '#map-view',
+            map: '#map-view map',
             settingsButton: '#map-view button[iconCls=settings]'
         },
         control: {
             mapView: {
                 show: 'updateTitleBar'
             },
+            map: {
+                maprender: 'drawDogPath'
+            },
             settingsButton: {
                 tap: 'redirectToSettings'
             }
-        }
+        },
+
+        locationStore: null
+    },
+
+    init: function() {
+        this.setLocationStore(Ext.create('Tasma.store.DogLocations'));
     },
 
     launch: function() {
@@ -35,5 +45,54 @@ Ext.define('Tasma.controller.Map', {
 
     redirectToSettings: function() {
         this.redirectTo('settings');
+    },
+
+    drawDogPath: function(mapComp) {
+        var geo = mapComp.getGeo();
+
+        geo.setAutoUpdate(false);
+
+        var gMap = mapComp.getMap();
+        var dogPath = new google.maps.Polyline({
+            geodesic: true,
+            strokeColor: '#FF0000',
+            icons: [{
+                icon: {path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW},
+                offset: '100%'
+            }],
+            map: gMap
+        });
+        var latLngBounds = new google.maps.LatLngBounds();
+
+
+        var lastLatLng = this.getLocationStore().first();
+
+        var startPoint = new google.maps.Marker({
+            position: new google.maps.LatLng(lastLatLng.get('lat'), lastLatLng.get('lng')),
+            map: gMap,
+            title: 'Buradasınız'
+        });
+
+        var updatePath = function(record) {
+            var point = new google.maps.LatLng(record.get('lat'), record.get('lng'));
+
+            dogPath.getPath().push(point);
+
+            latLngBounds.extend(point);
+        };
+
+        this.getLocationStore().each(updatePath);
+
+        gMap.fitBounds(latLngBounds);
+
+        this.getLocationStore().on('addrecords', function(store, records) {
+            records.forEach(updatePath);
+
+            gMap.fitBounds(latLngBounds);
+
+            var distance = google.maps.geometry.spherical.computeDistanceBetween(startPoint.getPosition(), dogPath.getPath().getAt(dogPath.getPath().getLength() - 1));
+
+            this.getMapView().down('titlebar').setTitle(Ext.util.Format.format('{0} sizden {1} metre uzakta', this.getApplication().dog.get('name'), Math.ceil(distance)));
+        }, this);
     }
 });
