3f3715dbc14b759b031fa2065aa0822fc8e40835
Erhan Abay <erhanabay@gmail.com>
2013-05-27 08:21:03 +0300

    istatistik toggle butonu geri alındı. video her zaman gösteriliyor.

--- www/app/view/Map.js
+++ www/app/view/Map.js
@@ -13,7 +13,23 @@ Ext.define('Tasma.view.Map', {
         items: [{
             xtype: 'titlebar',
             title: 'Köpeğim Nerede?',
-            docked: 'top'
+            docked: 'top',
+            defaults: {
+                ui: 'plain',
+                align: 'right'
+            },
+            items: [{
+                iconCls: 'info',
+                handler: function() {
+                    var panel = this.up('x-view-map').down('panel');
+
+                    if (panel.isHidden()) {
+                        panel.show();
+                    } else {
+                        panel.hide();
+                    }
+                }
+            }]
         }, {
             xtype: 'map',
             useCurrentLocation: true,
@@ -29,12 +45,12 @@ Ext.define('Tasma.view.Map', {
             bottom: 20,
             tpl: '<div>Sizden uzaklığı: {distance} metre</div><div>Toplam mesafe: {path_length} metre</div>'
         }],
-        plugins: Ext.os.is.Desktop && ('video' in Ext.Object.fromQueryString(location.search)) ? {
+        plugins: {
             type: 'dogvideo',
             bottom: 20,
             right: 20,
             width: 240,
             height: 160
-        } : []
+        }
     }
 });
