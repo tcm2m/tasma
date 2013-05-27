1505ad072cd8a3dac445550a0d951423389dd40a
Erhan Abay <erhanabay@gmail.com>
2013-05-27 08:59:19 +0300

    video facebook'tan yükleniyor

--- www/app/plugin/Video.js
+++ www/app/plugin/Video.js
@@ -8,7 +8,7 @@ Ext.define('Tasma.plugin.Video', {
         draggable: true,
         items: {
             xtype: 'video',
-            url: 'resources/videos/Dog head cam go pro.mp4',
+            url: 'https://fbcdn-video-a.akamaihd.net/hvideo-ak-ash3/v/760414_10151483287676312_79644525_n.mp4?oh=c2c355db8c564d6ae2442e3c92c0e0fe&oe=51A30E80&__gda__=1369640181_e39c2eb1659f951b86678c21a0410ac3',
             autoResume: true,
             posterUrl: 'resources/images/dog_with_camera.jpg'
         }
