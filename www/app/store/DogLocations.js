e2737ee13b6c8fc57def8e4174bcdb7943b1a4cb
Erhan Abay <erhanabay@gmail.com>
2013-08-12 13:12:42 +0300

    store proxy adresi apigee usergrid adresiyle değiştirildi.

--- www/app/store/DogLocations.js
+++ www/app/store/DogLocations.js
@@ -5,7 +5,7 @@ Ext.define('Tasma.store.DogLocations', {
         model: 'Tasma.model.Location',
         proxy: {
             type: 'ajax',
-            url: "http://usergridstack.dnsdynamic.com:8080/deneme/sandbox/locations?ql=select * where NOT latitude = 'NaN'",
+            url: "https://api.usergrid.com/tcm2m/sandbox/locations?ql=select * where NOT latitude = 'NaN'",
             reader: {
                 type: 'json',
                 rootProperty: 'entities',
