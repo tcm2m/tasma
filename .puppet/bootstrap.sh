d23d61b62a9abe481f0c56dfb6bb0f011ea02582
Erhan Abay <erhanabay@gmail.com>
2013-08-14 09:22:55 +0300

    librarian-puppet-maestrodev kurulumu artık dokümanlar haric, gereksinimler dahil olarak gerçekleştiriliyor

--- .puppet/bootstrap.sh
+++ .puppet/bootstrap.sh
@@ -8,7 +8,7 @@ if [ ! -f $BOOTSTRAP_LOCK ]; then
     apt-get install git -y
 
     if [ `gem query --local | grep librarian-puppet-maestrodev | wc -l` -eq 0 ]; then
-      gem install librarian-puppet-maestrodev
+      gem install librarian-puppet-maestrodev --include-dependencies --no-rdoc --no-ri
       cd $PUPPET_DIR && librarian-puppet install --clean
     fi
 
